fbf25e9bb9729dd03a9671f56822b44cf5371ae4
DTChung1912 <daicabadao317@gmail.com>
2022-03-09 15:21:32 +0700

    Xóa dóng thừa

--- app/src/main/java/com/example/toptracks/Fragment/purchasestracks/FragmentPurchasesPresenter.java
+++ app/src/main/java/com/example/toptracks/Fragment/purchasestracks/FragmentPurchasesPresenter.java
@@ -2,14 +2,10 @@ package com.example.toptracks.Fragment.purchasestracks;
 
 import com.example.toptracks.Model.Music;
 import com.example.toptracks.base.BasePresenter;
-
 import java.util.ArrayList;
 
 public class FragmentPurchasesPresenter extends BasePresenter<PurchasesIterator.PurchasesTrackView>
         implements PurchasesIterator.PurchasesTrackPresenter  {
-
-
-
     @Override
     public void fetchPurchasesTracks() {
         ArrayList<Music> musicList = new ArrayList<>();
